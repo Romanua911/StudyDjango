37821af453b033253a12f3643ab3a4f8570f90c4
romanua911 <kuc911@gmail.com>
2019-12-31 20:56:53 +0200

    rout

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,3 +19,16 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# Используйте include() чтобы добавлять URL из каталога приложения
+from django.urls import include
+from django.urls import path
+urlpatterns += [
+     path('catalog/', include('catalog.urls')),
+]
+
+# Добавьте URL соотношения, чтобы перенаправить запросы с корневового URL, на URL приложения
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
