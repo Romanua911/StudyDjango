6449d075060eeed051e27dfa1b21d8af4ed27483
romanua911 <kuc911@gmail.com>
2020-01-20 16:29:49 +0200

    Sessions

--- catalog/models.py
+++ catalog/models.py
@@ -113,3 +113,5 @@ class Author(models.Model):
         String for representing the Model object.
         """
         return '%s, %s' % (self.last_name, self.first_name)
+    class Meta:
+        ordering = ['last_name']
--- catalog/views.py
+++ catalog/views.py
@@ -14,12 +14,39 @@ def index(request):
     # Доступные книги (статус = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()  # Метод 'all()' применен по умолчанию.
+    num_genre = Genre.objects.all().count()
+    num_booksWithA = Book.objects.filter(title__contains='мага').count()
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
         request,
-        'index.html',
-        context={'num_books': num_books, 'num_instances': num_instances,
-                 'num_instances_available': num_instances_available, 'num_authors': num_authors},
+        'index.html', context={
+                    'num_books': num_books,
+                    'num_instances': num_instances,
+                    'num_instances_available': num_instances_available,
+                    'num_authors': num_authors,
+                    'num_genre': num_genre,
+                    'num_booksWithA': num_booksWithA,
+                    'num_visits': num_visits
+                 },
     )
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
